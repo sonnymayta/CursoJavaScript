06fd5ba08ba1067737b3d888ea3dc278bb465e0d
Sonny Mayta <mayta.sonny@gmail.com>
2024-02-22 14:19:48 -0400

    Declaración de variables

---
+++ variables.js
@@ -0,0 +1,10 @@
+// Declaración de variables
+let saludo = "Hola mundo";
+let numero = 1;
+let estado = true;
+let indefinido;
+console.log(saludo);
+console.log(typeof(saludo));
+console.log(typeof(numero));
+console.log(typeof(estado));
+console.log(typeof(indefinido));
