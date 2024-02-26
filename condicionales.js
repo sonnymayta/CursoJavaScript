2a00ec6bf2607e334ca52cfab95d802ab845a27b
Sonny Mayta <mayta.sonny@gmail.com>
2024-02-26 09:44:00 -0400

    Condicionales

---
+++ condicionales.js
@@ -0,0 +1,28 @@
+// Sentencia IF
+let operacion = 5 + 2;
+let valor = 5;
+if (operacion >= 6) {
+  console.log("Es true");
+} else {
+  console.log("Es false");
+}
+
+// Uso del else if
+if (valor > 1) {
+
+} else if (valor < 2 && valor === 1) {
+
+} else if (valor = 5) {
+
+}
+
+// Sentencia SWITCH
+switch(operacion) {
+  case 0:
+    console.log("El resultado es 0");
+    break;
+  case 7:
+    console.log("El resultado es 7");
+  default:
+    console.log("El resultado no es 0 o 7")
+}
