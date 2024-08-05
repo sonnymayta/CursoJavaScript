80d70165c53b3c0292688e276160c3f35a94a6d9
Sonny Mayta <mayta.sonny@gmail.com>
2024-08-05 12:30:12 -0400

    Eloquent exercise - Fizzbuzz

---
+++ Eloquent-JavaScript/exercises/fizzbuzz.js
@@ -0,0 +1,30 @@
+/*
+  Write a program that uses console.log to print all the numbers from 1
+  to 100, with two exceptions. For numbers divisible by 3, print "Fizz"
+  instead of the number, and for numbers divisible by 5 (and not 3), print
+  "Buzz" instead.
+  When you have that working, modify your program to print "FizzBuzz
+  " for numbers that are divisible by both 3 and 5 (and still print "Fizz"
+  or "Buzz" for numbers divisible by only one of those).
+  (This is actually an interview question that has been claimed to weed
+  out a significant percentage of programmer candidates. So if you solved
+  it, your labor market value just went up.)
+*/
+
+for (let number = 1; number <= 100; number++) {
+  if (number % 3 == 0 && number % 5 == 0) {
+    console.log(number + " - FizzBuzz");
+  }
+  else {
+    if (number % 3 == 0) {
+      console.log(number + " - Fizz");
+    }
+    else if (number % 5 == 0) {
+      console.log(number + " - Buzz");
+    }
+    else {
+      console.log(number + " - " + number);
+    }
+  }
+  
+}
